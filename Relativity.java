d74c75460c84b2626e414699ede96b76a2d4dc2a
AmitRoy3370 <70816613+AmitRoy3370@users.noreply.github.com>
2021-07-02 16:00:58 +0600

    Add files via upload

---
+++ Relativity.java
@@ -0,0 +1,94 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class Relativity {
+
+      static Scanner in=new Scanner();
+    
+    static PrintWriter out=new PrintWriter(System.out);
+    
+    static int testCases;
+    
+    static long g,c;
+    
+    static void solve(){
+        
+        long ans=(c*c)/(2*g);
+        
+        out.println(ans);
+        out.flush();
+        
+    }
+    
+    public static void main(String[] amit) throws IOException {
+        
+        testCases=in.nextInt();
+        
+        for(int t=0;t<testCases;t++){
+            
+            g=in.nextLong();
+            c=in.nextLong();
+            
+            solve();
+            
+        }
+        
+    }
+    
+      static class Scanner{
+        
+        BufferedReader in;
+        StringTokenizer st;
+
+        public Scanner() {
+        
+            in=new BufferedReader( new InputStreamReader( System.in ) );
+            
+        }
+        
+        String next() throws IOException{
+            
+            while( st==null || !st.hasMoreElements() ){
+                
+                st=new StringTokenizer(in.readLine());
+                
+            }
+            
+            return st.nextToken();
+            
+        }
+        
+        String nextLine() throws IOException{
+            
+            return in.readLine();
+            
+        }
+        
+        int nextInt() throws IOException{
+            
+            return Integer.parseInt( next() );
+            
+        }
+        
+        long nextLong() throws IOException{
+            
+            return Long.parseLong( next() );
+            
+        }
+        
+    }
+    
+}
+
+/*
+
+3
+7 1400
+5 1000
+10 1000
+
+*/
