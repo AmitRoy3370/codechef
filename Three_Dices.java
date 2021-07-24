7c4ac55c5066853df9b7f6f241714ad5cf821a37
AmitRoy3370 <70816613+AmitRoy3370@users.noreply.github.com>
2021-07-24 16:38:05 +0600

    Add files via upload

---
+++ Three_Dices.java
@@ -0,0 +1,132 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class Three_Dices {
+
+                 static Scanner in=new Scanner();
+    
+    static PrintWriter out=new PrintWriter( new OutputStreamWriter(System.out) );
+
+    static int testCases,x,y;
+    
+    static void solve(){
+        
+       double prob=(6.0-(x+y))/6.0;
+        
+       if(prob>0){
+           
+           out.println(prob);
+           out.flush();
+           
+       }else{
+        
+           out.println(0);
+           out.flush();
+       
+       }
+           
+    }
+    
+    public static void main(String[] amit) throws IOException {
+        
+        testCases=in.nextInt();
+        
+        for(int t=0;t<testCases;t++){
+            
+            x=in.nextInt();
+            y=in.nextInt();
+            
+            solve();
+            
+        }
+        
+    }
+ 
+      static class Scanner{
+        
+        BufferedReader in;
+        StringTokenizer st;
+
+        public Scanner() {
+        
+            in=new BufferedReader( new InputStreamReader(System.in) );
+            
+        }
+        
+        String next() throws IOException{
+            
+            while(st==null || !st.hasMoreElements()){
+                
+                st=new StringTokenizer(in.readLine());
+                
+            }
+            
+            return st.nextToken();
+            
+        }
+        
+        int nextInt() throws IOException{
+            
+            return Integer.parseInt(next());
+            
+        }
+        
+        long nextLong() throws IOException{
+            
+            return Long.parseLong(next());
+            
+        }
+        
+        String nextLine() throws IOException{
+            
+            return in.readLine();
+            
+        }
+        
+        char nextChar() throws IOException{
+            
+            return next().charAt(0);
+            
+        }
+        
+        double nextDouble() throws IOException{
+            
+            return Double.parseDouble(next());
+            
+        }
+        
+        float nextFloat() throws IOException{
+            
+            return Float.parseFloat(next());
+            
+        }
+        
+        boolean nextBoolean() throws IOException{
+            
+            return Boolean.parseBoolean(next());
+            
+        }
+        
+        void close() throws IOException{
+            
+            in.close();
+            
+        }
+        
+    }
+   
+}
+/*
+
+3
+1 3
+2 4
+2 3
+
+
+*/
