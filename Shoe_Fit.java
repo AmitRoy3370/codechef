8cbff76458d79b56f8ee63fb43a317115b18bdcc
AmitRoy3370 <70816613+AmitRoy3370@users.noreply.github.com>
2021-07-28 20:41:06 +0600

    Add files via upload

---
+++ Shoe_Fit.java
@@ -0,0 +1,131 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class Shoe_Fit {
+
+               static Scanner in=new Scanner();
+    
+    static PrintWriter out=new PrintWriter( new OutputStreamWriter(System.out) );
+
+    static int testCases,a,b,c;
+    
+    static void solve(){
+        
+        if(a*b*c==0 && a+b+c>0 ){
+            
+            out.println(1);
+            out.flush();
+            
+        }else{
+            
+            out.println(0);
+            out.flush();
+            
+        }
+        
+    }
+    
+    public static void main(String[] amit) throws IOException {
+        
+        testCases=in.nextInt();
+        
+        for(int t=0;t<testCases;t++){
+            
+            a=in.nextInt();
+            b=in.nextInt();
+            c=in.nextInt();
+            
+            solve();
+            
+        }
+        
+    }
+    
+     static class Scanner{
+        
+        BufferedReader in;
+        StringTokenizer st;
+
+        public Scanner() {
+        
+            in=new BufferedReader( new InputStreamReader(System.in) );
+            
+        }
+        
+        String next() throws IOException{
+            
+            while(st==null || !st.hasMoreElements()){
+                
+                st=new StringTokenizer(in.readLine());
+                
+            }
+            
+            return st.nextToken();
+            
+        }
+        
+        int nextInt() throws IOException{
+            
+            return Integer.parseInt(next());
+            
+        }
+        
+        long nextLong() throws IOException{
+            
+            return Long.parseLong(next());
+            
+        }
+        
+        String nextLine() throws IOException{
+            
+            return in.readLine();
+            
+        }
+        
+        char nextChar() throws IOException{
+            
+            return next().charAt(0);
+            
+        }
+        
+        double nextDouble() throws IOException{
+            
+            return Double.parseDouble(next());
+            
+        }
+        
+        float nextFloat() throws IOException{
+            
+            return Float.parseFloat(next());
+            
+        }
+        
+        boolean nextBoolean() throws IOException{
+            
+            return Boolean.parseBoolean(next());
+            
+        }
+        
+        void close() throws IOException{
+            
+            in.close();
+            
+        }
+        
+    }
+    
+}
+/*
+
+3
+0 0 0
+0 1 1
+1 0 1
+
+
+*/
