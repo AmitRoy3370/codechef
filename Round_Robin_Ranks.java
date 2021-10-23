20fdbf26dd520fe67a9697e242be8e20fd7e09c7
AmitRoy3370 <70816613+AmitRoy3370@users.noreply.github.com>
2021-10-23 23:18:23 +0600

    Add files via upload

---
+++ Round_Robin_Ranks.java
@@ -0,0 +1,141 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class Round_Robin_Ranks {
+	
+    static Scanner in=new Scanner();
+    
+    static PrintWriter out=new PrintWriter( new OutputStreamWriter(System.out) );
+
+    static int testCases;
+    
+    static long n,k;
+    
+    static void solve(){
+        
+        long ans=(2*n-k-1)/2;
+        
+        out.println(ans*2);
+        out.flush();
+        
+    }
+    
+    public static void main(String[] amit) throws IOException {
+        
+        testCases=in.nextInt();
+        
+        for(int t=0;t<testCases;t++){
+            
+            n=in.nextLong();
+            k=in.nextLong();
+            
+            solve();
+            
+        }
+        
+        in.close();
+        
+    }
+    
+    static long gcd(long a, long b)   
+{   
+  
+if (a == 0)   
+return b;   
+if (b == 0)   
+return a;   
+if (a == b)   
+return a;   
+if (a > b)   
+return gcd(a-b, b);   
+return gcd(a, b-a);   
+}   
+    
+	  static class Scanner{
+        
+        BufferedReader in;
+        StringTokenizer st;
+
+        public Scanner() {
+        
+            in=new BufferedReader( new InputStreamReader(System.in) );
+            
+        }
+        
+        String next() throws IOException{
+            
+            while(st==null || !st.hasMoreElements()){
+                
+                st=new StringTokenizer(in.readLine());
+                
+            }
+            
+            return st.nextToken();
+            
+        }
+        
+        int nextInt() throws IOException{
+            
+            return Integer.parseInt(next());
+            
+        }
+        
+        long nextLong() throws IOException{
+            
+            return Long.parseLong(next());
+            
+        }
+        
+        String nextLine() throws IOException{
+            
+            return in.readLine();
+            
+        }
+        
+        char nextChar() throws IOException{
+            
+            return next().charAt(0);
+            
+        }
+        
+        double nextDouble() throws IOException{
+            
+            return Double.parseDouble(next());
+            
+        }
+        
+        float nextFloat() throws IOException{
+            
+            return Float.parseFloat(next());
+            
+        }
+        
+        boolean nextBoolean() throws IOException{
+            
+            return Boolean.parseBoolean(next());
+            
+        }
+        
+		
+        void close() throws IOException{
+            
+            in.close();
+            
+        }
+        
+    }
+	
+}
+/*
+
+3
+3 3
+4 1
+7 4
+
+
+*/
